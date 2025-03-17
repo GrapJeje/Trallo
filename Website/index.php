65f98808355d34af275ab8caba9d415e88d62cdf
martijn07 <dmb0070@edu.curio.nl>
2025-03-17 09:21:07 +0100

    added linked header and footer

--- Website/index.php
+++ Website/index.php
@@ -10,11 +10,12 @@
 </head>
 
 <body>
-    <?php require_once 'auth/loginHeader.php'; ?>
+    <?php require_once 'layout/header.php' ?>
 
     <?php if (isset($_GET['msg'])): ?>
         <p><?php echo $_GET['msg']; ?></p>
     <?php endif; ?>
+    <?php require_once 'layout/footer.php' ?>
 </body>
 
 </html>
