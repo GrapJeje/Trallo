59e8d615cbf5ee2ac953903d998bb8abee70080f
Jason <jasonvanl2019@gmail.com>
2025-03-18 08:33:02 +0100

    Added update backend

--- Website/backend/Controllers/tasksController.php
+++ Website/backend/Controllers/tasksController.php
@@ -2,6 +2,8 @@
 global $conn, $base_url;
 session_start();
 
+require_once "../conn.php";
+
 $action = $_POST["action"];
 $user_id = $_SESSION["user_id"];
 
@@ -14,8 +16,6 @@ if ($action === "create")
     $status = $_POST["status"];
 
     // Voeg taak toe
-    require_once "../conn.php";
-
     $query = "INSERT INTO planning_board (user_id, title, description, section, status)
     VALUES(:user_id, :title, :description, :section, :status)";
 
@@ -34,6 +34,27 @@ if ($action === "create")
 }
 else if ($action == "update")
 {
+    // Bewaar form data in variabelen
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+    $section = $_POST["section"];
+    $status = $_POST["status"];
+    $id = $_POST["id"];
+
+    $query = "UPDATE planning_board SET title = :title, description = :description, section = :section, status = :status 
+                      WHERE id = :id AND user_id = :user_id";
+
+    $statement = $conn->prepare($query);
+
+    $statement->execute([
+        ":user_id" => $user_id,
+        ":title" => $title,
+        ":description" => $description,
+        ":section" => $section,
+        ":status" => $status,
+        ":id" => $id,
+    ]);
+
     header("Location: $base_url/tasks");
     exit();
 }
