8f33d392fa924a9db5e10cbd6470bfdc6badd490
Jason <jasonvanl2019@gmail.com>
2025-04-01 13:45:04 +0200

    Small header tweaks

--- Website/auth/loginHeader.php
+++ Website/auth/loginHeader.php
@@ -6,10 +6,17 @@ if (session_status() === PHP_SESSION_NONE) {
 require_once __DIR__ . '/../backend/config.php';
 ?>
 
-<div>
+<div class="login-container">
     <?php if(isset($_SESSION['user_id'])): ?>
         <a href="<?php echo $base_url; ?>/auth/logout.php">Uitloggen</a>
     <?php else: ?>
         <a href="<?php echo $base_url; ?>/login">Inloggen</a>
+    <?php endif; ?>
+</div>
+<div class="register-container">
+    <?php if (isset($_SESSION['user_id'])): ?>
+        <a href="<?php echo $base_url; ?>/tasks">Dashboard</a>
+    <?php else : ?>
+        <a href="<?php echo $base_url; ?>/register">Register</a>
     <?php endif; ?>
 </div>
--- Website/layout/header.php
+++ Website/layout/header.php
@@ -7,15 +7,10 @@ require_once __DIR__ . "/../backend/conn.php";
 
 <header>
     <h1>Trallo<span>Dashboard</span></h1>
-    <div>
+    <a href="<?php echo $base_url?>">
         <img src="<?php echo $base_url ?>/public/img/logo-big-v4.png" alt="Logo">
-    </div>
+    </a>
     <div class="nav-container">
-        <div class="login-container">
-            <?php require_once __DIR__ . '/../auth/loginHeader.php'; ?>
-        </div>
-        <div class="register-container">
-            <a href="<?php echo $base_url; ?>/register">Register</a>
-        </div>
+        <?php require_once __DIR__ . "/../auth/loginHeader.php"; ?>
     </div>
 </header>
