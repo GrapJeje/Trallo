4ea5582668d0d77eb3ad5a27c16fa07e5436ec1b
Jason <jasonvanl2019@gmail.com>
2025-04-01 15:14:12 +0200

    Added styling to the front page

--- Website/index.php
+++ Website/index.php
@@ -5,15 +5,25 @@
     <?php require_once "layout/head.php"; ?>
     <title>Trallo</title>
     <link rel="stylesheet" href="public/css/main.css">
+    <link rel="stylesheet" href="public/css/home.css">
 </head>
 
 <body>
-    <?php require_once 'layout/header.php'; ?>
+<?php require_once 'layout/header.php' ?>
 
-    <?php if (isset($_GET['msg'])): ?>
-        <p><?php echo $_GET['msg']; ?></p>
-    <?php endif; ?>
-    <?php require_once 'layout/footer.php' ?>
+<main>
+    <div class="main-card">
+        <img src="public/img/logo-big-v2.png" alt="Developer Land">
+        <div class="main-content">
+            <p>Heb jij ook altijd last van onduidelijkheid bij taakverdeling in je team?</p>
+            <p><strong>Meld je dan nu aan bij Trallo!</strong></p>
+            <p>Het takenbord voor handige werkers.</p>
+            <a href="register" class="signup-button">Meld je aan!</a>
+        </div>
+    </div>
+</main>
+
+<?php require_once 'layout/footer.php' ?>
 </body>
 
 </html>
